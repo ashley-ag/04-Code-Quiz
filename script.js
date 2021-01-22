1a3ea993c06e807d8b9f59346c92c8316a2c8f02
AG-gash <74515756+ashley-ag@users.nereply.github.com>
2021-01-21 17:30:45 -0800

    Second rev with more JS added

--- script.js
+++ script.js
@@ -1,53 +1,63 @@
-document.readyState(
-    $("#question1").hide();
-    $("#question2").hide();
-    $("#question3").hide();
-    $("#highScores").hide();
+var timer = 60;
+var score = 0;
+var endScore = '';
+var userInitals = '';
+var currentQuestion = 1;
 
-    // GIVEN I am taking a code quiz
-    // WHEN I click the start button
-    $("#startButton").on("click", function {
+//#startButton.on("click," function must start timer in #timer div and flip to #question1 div)
+$("#startButton").on("click", function() {
+    // Timer needs to show
+    $("#timer").removeClass("hidden");
+    // Timer needs to start
+    setTime();
+    // Next question needs to show 
+    // start page needs to hide
+    $("#startPage").addClass("hidden");
+    $("#question" + currentQuestion).removeClass("hidden");
+})
 
-        $("#startPage").hide();
-        $("#question2").show();
-        setTime();
+//clicked on the wrong answer "incorrect" populates
+$(".answer").on("click", function() {
+    if ($(this).data("answer") === $(this).parent().data("answer")) {
+        //console.log("the answer is correct!")
+        //add 10 points to score
+        score += 10;
+        //move to next question by doing the following:
+        $("#question" + currentQuestion).addClass("hidden"); 
+        currentQuestion++; 
+        $("#question" + currentQuestion).removeClass("hidden"); 
+    } else {
+        console.log("answer is wrong.")
+    //time -= 10
+    //flip to next question
+    //hide current question
+    }
+})
+
+//time -= 10
+//flip to next question
+//hide current question
+
+
+
+////use an if statement that addresses all of the question divs
+
+
+//if the question was answered correctly "Correct!" must show on the screen, score +=10, flip to next question or #endPage div
+
+//if timer===0 then game over flip to #endPage div or if all questions have been answered
+
+//#endPage div must show your score, enter your intials, show score next to intials and save score
+////use a for loop, .append(score), and local storage
+
+
 
 function setTime() {
-var secondsLeft = 60;
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    $().text = secondsLeft + " seconds left till colorsplosion.";
-
-    if(secondsLeft === 0) {
-      // Stops execution of action at set interval
-      clearInterval(timerInterval);
-      // Calls function to create and append image
-      sendMessage();
-    }
-
-  }, 1000);
+    var timerInterval = setInterval(function() {
+        $("#timeLeft").text(timer);
+        timer--;
+        if(timer === 0) {
+            clearInterval(timerInterval);
+        }
+    }, 1000)
 }
-    })
-
-    // THEN a timer starts and I am presented with a question
-
-
-    // WHEN I answer a question
-    // THEN I am presented with another question
-
-
-    // WHEN I answer a question incorrectly
-    // THEN time is subtracted from the clock
-
-
-    // WHEN all questions are answered or the timer reaches 0
-    // THEN the game is over
-
-
-
-    // WHEN the game is over
-    // THEN I can save my initials and my score
-
-
-
-)
