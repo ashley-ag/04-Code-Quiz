7244c678855a5956ee7d6230a016de5d8749c936
AG-gash <74515756+ashley-ag@users.nereply.github.com>
2021-01-25 21:21:32 -0800

    Final commit of code

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ var score = 0;
 var endScore = '';
 var userInitals = '';
 var currentQuestion = 1;
+var timerInterval; 
+var scoresFromStorage = JSON.parse(localStorage.getItem("scores"));
+var highScores = [];
+
+
 
 //#startButton.on("click," function must start timer in #timer div and flip to #question1 div)
 $("#startButton").on("click", function() {
@@ -18,78 +23,82 @@ $("#startButton").on("click", function() {
 
 //clicked on the wrong answer "incorrect" populates
 $(".answer").on("click", function() {
+    //debugger;
     if ($(this).data("answer") === $(this).parent().data("answer")) {
         //console.log("the answer is correct!")
         //add 10 points to score
         score += 10;
+        $("#feedback").removeClass("hidden"); 
+        $("#feedback").addClass("correct");
+        $("#feedback").text("CORRECT");
         //move to next question by doing the following:
         $("#question" + currentQuestion).addClass("hidden"); 
         currentQuestion++; 
         $("#question" + currentQuestion).removeClass("hidden"); 
+        setTimeout(function(){
+            $("#feedback").addClass("hidden");
+            $("#feedback").removeClass("correct");
+        }, 1000)
     } else {
        //console.log("answer is wrong.")
         timer -= 10;
+        $("#feedback").removeClass("hidden");
+        $("#feedback").addClass("incorrect");
+        $("#feedback").text("INCORRECT");
         //flip to next question
         //hide current question
         $("#question" + currentQuestion).addClass("hidden"); 
         currentQuestion++; 
         $("#question" + currentQuestion).removeClass("hidden"); 
+        setTimeout(function(){
+            $("#feedback").addClass("hidden");
+            $("#feedback").removeClass("correct");
+        }, 1000)
+    }
+    if (currentQuestion === 4) {
+        //debugger;
+        console.log("yay we are here");
+        $("#timer").addClass("hidden");
+        $("#score").text(score);
+        $("#scorePage").removeClass("hidden");
+        clearInterval(timerInterval);
     }
 })
 
 //timer must hide
 //score needs to show
 
-if (currentQuestion === 2) {
-    $("#timer").addClass("hidden");
-    clearInterval(timerInterval);
-}
-
-
-
-var scoresFromStorage = JSON.parse(localStorage.getItem("scores"));
-
 if (!scoresFromStorage){
-    scoresFromStorage = [];
+    highScores = [];
+} else {
+    highScores = scoresFromStorage;
 }
 
-for (var i = 0; i < scoresFromStorage.length; i++){
-    var paragraph = document.createElement("p");
-
-    paragraph.innerText = scoresFromStorage[i].name + "'s score is " + scoresFromStorage[i].score;
-
-    document.body.appendChild(paragraph);
-}
-
-var storeButton = document.getElementById("store");
+var storeButton = document.getElementById("submit");
 var nameInput =  document.getElementById("initials");
 var scoreInput =  document.getElementById("score");
 
 storeButton.addEventListener("click", function(){
     var name = nameInput.value;
-    var score = scoreInput.value;
 
     var newLocalStorageEntry = {
         name: name,
-        score: score
+        score: score,
     };
 
-    scoresFromStorage.push(newLocalStorageEntry);
+    highScores.push(newLocalStorageEntry);
 
-    localStorage.setItem("scores", JSON.stringify(scoresFromStorage));
+    localStorage.setItem("scores", JSON.stringify(highScores));
+
+    var paragraph = document.createElement("p");
+    paragraph.innerText = newLocalStorageEntry.name + "'s score is " + newLocalStorageEntry.score;
+    document.body.appendChild(paragraph);
 })
-
-//if the question was answered correctly "Correct!" must show on the screen, score +=10, flip to next question or #endPage div
-
-//if timer===0 then game over flip to #endPage div or if all questions have been answered
-
-//#endPage div must show your score, enter your intials, show score next to intials and save score
-////use a for loop, .append(score), and local storage
 
 
 
 function setTime() {
-    var timerInterval = setInterval(function() {
+    timerInterval = setInterval(function() {
         $("#timeLeft").text(timer);
         timer--;
         if(timer === 0) {
